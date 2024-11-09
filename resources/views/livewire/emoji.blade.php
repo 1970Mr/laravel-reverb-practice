be4ea675bbdb12071911900a4b8dabf318b56c9a
1970Mr <mrepoch1970@gmail.com>
2024-11-10 02:15:53 +0330

    Add initial style

--- resources/views/livewire/emoji.blade.php
+++ resources/views/livewire/emoji.blade.php
@@ -1,6 +1,8 @@
 <div class="min-h-[100vh] flex justify-center items-center bg-[#110601] text-[#FFF5F0]">
-    <div class="bg-black p-3 rounded-full flex border-[.1rem] border-gray-400 backdrop-blur-3xl">
-        <div>Text</div>
-        <div>Text</div>
+    <div class="bg-black p-5 rounded-full flex border-[.1rem] border-gray-400 backdrop-blur-3xl gap-5">
+        <div class="text-5xl cursor-pointer p-2 transition hover:scale-125" wire:click="sendReaction('heart')">❤️</div>
+        <div class="text-5xl cursor-pointer p-2 transition hover:scale-125" wire:click="sendReaction('fire')">🔥</div>
+        <div class="text-5xl cursor-pointer p-2 transition hover:scale-125" wire:click="sendReaction('rocket')">🚀</div>
+        <div class="text-5xl cursor-pointer p-2 transition hover:scale-125" wire:click="sendReaction('wonder')">🤯</div>
     </div>
 </div>
